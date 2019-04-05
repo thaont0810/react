e313d7ea6006f3a2055e3a272837144b48829e2c
ThaoNT <thaont@rikkeisoft.com>
2019-04-05 08:24:45 +0700

    calculator done

--- react-training/src/components/Calc.js
+++ react-training/src/components/Calc.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../calc.css';
 import Result from './Result'
 import KeyNumber from "./KeyNumber";
@@ -7,41 +7,56 @@ class Calc extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            result: '0',
+            result: '0'
         };
         // this.handleClick = this.handleClick.bind(this);
     }
 
-    handleClick = (value) => {
-        if (value === '=') {
+    handleClick = (btn) => {
+        if (btn === '=') {
             this.calculate();
-        }
-        else if (value === 'C') {
+        } else if (btn === 'C') {
             this.reset();
-        }
-        else if (value === 'CE') {
+        } else if (btn === 'CE') {
             this.clear();
-        }
-        else {
-           if (this.state.result === '0' && value !== '0') {
-               this.setState({
-                   result: value
-               })
-           }
-           else if (this.state.result !== '0') {
-               this.setState({
-                   result: this.state.result + value
-               })
-           }
+        } else {
+            if (this.state.result === '0' && btn !== '0') {
+                this.setState({
+                    result: btn
+                })
+            } else if (this.state.result !== '0') {
+                this.setState({
+                    result: this.state.result + btn
+                })
+            }
 
         }
     };
+
 
     calculate = () => {
-        this.setState({
-            result: eval(this.state.result)
-        })
+        const {result} = this.state;
+
+        let checkResult = '';
+        if (result.includes('--')) {
+            checkResult = result.replace('--', '+')
+        } else {
+            checkResult = result
+        }
+
+        try {
+            this.setState({
+                // turn result to string
+                result: (eval(checkResult)) + ""
+            })
+
+        } catch (e) {
+            this.setState({
+                result: 'error'
+            })
+        }
     };
+
 
     reset = () => {
         this.setState({
@@ -50,11 +65,11 @@ class Calc extends Component {
     };
 
     clear = () => {
-        const { result } = this.state;
+        const {result} = this.state;
 
         this.setState({
-            result: result.slice(0,-1)
-            // result: result.substring(0, result.length - 1)
+            result: result.slice(0, -1) || '0'
+            // result: result.substring(0, result.length - 1) || '0'
         })
     };
 
@@ -62,8 +77,8 @@ class Calc extends Component {
 
         return (
             <div className={'container'}>
-                <Result result = {this.state.result}/>
-                <KeyNumber onClick = { e => this.handleClick(e)}/>
+                <Result result={this.state.result}/>
+                <KeyNumber onClick={e => this.handleClick(e)}/>
             </div>
         )
     }
--- react-training/src/components/KeyNumber.js
+++ react-training/src/components/KeyNumber.js
@@ -1,37 +1,41 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class KeyNumber extends Component {
-  constructor(props) {
-    super(props);
-  };
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        }
+    };
 
-  render() {
-    return (
-     <div>
-       <button className={'reset'} value={'C'} onClick={e => this.props.onClick(e.target.value)}>C</button>
-       <button className={'clear'} value={'CE'} onClick={e => this.props.onClick(e.target.value)}>CE</button>
-       <button className={'oper divide'} value={'/'} onClick={e => this.props.onClick(e.target.value)}>/</button>
+    render() {
+        const {onClick} = this.props;
+        return (
+            <div>
+                <button className={'reset'} value={'C'} onClick={e => onClick(e.target.value)}>C</button>
+                <button className={'clear'} value={'CE'} onClick={e => onClick(e.target.value)}>CE</button>
+                <button className={'oper divide'} value={'/'} onClick={e => onClick(e.target.value)}>/</button>
 
-       <button className={'num'} value={'7'} onClick={e => this.props.onClick(e.target.value)}>7</button>
-       <button className={'num'} value={'8'} onClick={e => this.props.onClick(e.target.value)}>8</button>
-       <button className={'num'} value={'9'} onClick={e => this.props.onClick(e.target.value)}>9</button>
-       <button className={'oper multiply'} value={'*'} onClick={e => this.props.onClick(e.target.value)}>x</button>
+                <button className={'num'} value={'7'} onClick={e => onClick(e.target.value)}>7</button>
+                <button className={'num'} value={'8'} onClick={e => onClick(e.target.value)}>8</button>
+                <button className={'num'} value={'9'} onClick={e => onClick(e.target.value)}>9</button>
+                <button className={'oper multiply'} value={'*'} onClick={e => onClick(e.target.value)}>x</button>
 
-       <button className={'num'} value={'4'} onClick={e => this.props.onClick(e.target.value)}>4</button>
-       <button className={'num'} value={'5'} onClick={e => this.props.onClick(e.target.value)}>5</button>
-       <button className={'num'} value={'6'} onClick={e => this.props.onClick(e.target.value)}>6</button>
-       <button className={'oper minus'} value={'-'} onClick={e => this.props.onClick(e.target.value)}>-</button>
+                <button className={'num'} value={'4'} onClick={e => onClick(e.target.value)}>4</button>
+                <button className={'num'} value={'5'} onClick={e => onClick(e.target.value)}>5</button>
+                <button className={'num'} value={'6'} onClick={e => onClick(e.target.value)}>6</button>
+                <button className={'oper minus'} value={'-'} onClick={e => onClick(e.target.value)}>-</button>
 
-       <button className={'num'} value={'1'} onClick={e => this.props.onClick(e.target.value)}>1</button>
-       <button className={'num'} value={'2'} onClick={e => this.props.onClick(e.target.value)}>2</button>
-       <button className={'num'} value={'3'} onClick={e => this.props.onClick(e.target.value)}>3</button>
-       <button className={'oper plus'} value={'+'} onClick={e => this.props.onClick(e.target.value)}>+</button>
+                <button className={'num'} value={'1'} onClick={e => onClick(e.target.value)}>1</button>
+                <button className={'num'} value={'2'} onClick={e => onClick(e.target.value)}>2</button>
+                <button className={'num'} value={'3'} onClick={e => onClick(e.target.value)}>3</button>
+                <button className={'oper plus'} value={'+'} onClick={e => onClick(e.target.value)}>+</button>
 
-       <button className={'num zero'} value={'0'} onClick={e => this.props.onClick(e.target.value)}>0</button>
-       <button className={'oper equal'} value={'='} onClick={e => this.props.onClick(e.target.value)}>=</button>
-     </div>
-    );
-  }
+                <button className={'num zero'} value={'0'} onClick={e => onClick(e.target.value)}>0</button>
+                <button className={'oper equal'} value={'='} onClick={e => onClick(e.target.value)}>=</button>
+            </div>
+        );
+    }
 }
 
 export default KeyNumber;
